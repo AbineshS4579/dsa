1f7eca7242344c89ac74a7ae02bcc288fb8c320b
Abinesh S <abinesh781845@gmail.com>
2026-08-29 18:10:25 +0530

    ascci to binary

---
+++ src/ascii.java
@@ -0,0 +1,10 @@
+public class ascii {
+
+    public static void main(String[] args){
+        char a='a',b='b';
+        int i=a,j='b';
+        String f=Integer.toBinaryString(i);
+        String k=Integer.toBinaryString(j);
+        System.out.println(f+k);
+    }
+}
